fa07ec280d25ddc83865b43cf2bfc5f5aa914d4a
Corey Applegate <cpapplefamily@gmail.com>
2024-07-21 11:16:48 -0500

    removed old

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,6 @@
 #define FASTLED_INTERNAL        // Suppress build banner
 #include <FastLED.h>
 
-#define OLED_CLOCK  15          // Pins for the OLED display
-#define OLED_DATA   4
-#define OLED_RESET  16
-
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C g_OLED(U8G2_R2, OLED_RESET, OLED_CLOCK, OLED_DATA);
 int g_lineHeight = 0;
 int g_Brightness = 5;//15;         // 0-255 LED brightness scale
 int g_PowerLimit = 50000;//900;        // 900mW Power Limit
@@ -209,7 +204,7 @@ void intiWifi(){
 
 void setup() {
 	
-	pinMode(LED_BUILTIN, OUTPUT);
+	//pinMode(LED_BUILTIN, OUTPUT);
   	pinMode(LED_PIN, OUTPUT);
 	// USE_SERIAL.begin(921600);
 	USE_SERIAL.begin(115200);
@@ -218,14 +213,10 @@ void setup() {
 	USE_SERIAL.setDebugOutput(true);
 
 	USE_SERIAL.println("set up oled");
-	g_OLED.begin();
-  	g_OLED.clear();
-  	g_OLED.setFont(u8g2_font_profont15_tf);
-  	g_lineHeight = g_OLED.getFontAscent() - g_OLED.getFontDescent();        // Descent is a negative number so we add it to the total
-
+	
 	FastLED.addLeds<WS2812B, LED_PIN, GRB>(g_LEDs, NUM_LEDS);               // Add our LED strip to the FastLED library
 	FastLED.setBrightness(g_Brightness);
-  	set_max_power_indicator_LED(LED_BUILTIN);                               // Light the builtin LED if we power throttle
+  	//set_max_power_indicator_LED(LED_BUILTIN);                               // Light the builtin LED if we power throttle
   	FastLED.setMaxPowerInMilliWatts(g_PowerLimit);                          // Set the power limit, above which brightness will be throttled
 
 	intiWifi();
@@ -381,16 +372,7 @@ void loop() {
 	FastLED.show(g_Brightness); //  Show and delay
 	
 	EVERY_N_MILLISECONDS(250){
-      g_OLED.clearBuffer();
-      g_OLED.setCursor(0, g_lineHeight);
-      g_OLED.printf("FPS  : %u", FastLED.getFPS());
-      g_OLED.setCursor(0, g_lineHeight * 2);
-      g_OLED.printf("Power: %u mW", calculate_unscaled_power_mW(g_LEDs, 4));
-      g_OLED.setCursor(0, g_lineHeight * 3);
-      g_OLED.printf("Brite: %d", calculate_max_brightness_for_power_mW(g_Brightness, g_PowerLimit));
-      g_OLED.setCursor(0, g_lineHeight * 4);
-      g_OLED.printf("Local ip: %s", WiFi.localIP());
-      g_OLED.sendBuffer();
+     
 	  USE_SERIAL.println(WiFi.status()); 
     }
 }
